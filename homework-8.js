15a37e946f119fd658cf9e3a4658f6f749f974f6
Sulayman <ushurovmetkiysulik@gmail.com>
2026-08-01 21:27:50 +0300

    домашнее задание №8

---
+++ homework-8.js
@@ -0,0 +1,118 @@
+// объект с личной информацией
+const person = {
+  name: "Sulayman",
+  surname: "Ushurov",
+  age: 19,
+  country: "Turkey",
+  city: "Istambul",
+  email: "ushurovmetkiysulik@gmail.com"
+}
+console.log(person);
+
+
+
+// информация автомобиля
+const car = {
+  brand: "Dodge Challenger",
+  model: "SRT Hellcat",
+  year: 2008,
+  color: "black",
+  engine: "6.2",
+  transmission: "automatic",
+}
+console.log(car)
+
+
+
+// мксимальная скорасть
+  function ensureMaxSpeed(car) {
+    if (car.hasOwnProperty("maxSpeed")) {
+        return;
+    }
+car.maxSpeed = 390;
+}
+
+console.log(ensureMaxSpeed)
+
+
+// #6 модель
+function showProperty(obj, property) {
+  console.log(obj[property]);
+}
+
+
+//№7 массив
+const products = ["молоко", "хлеб", "мясо", "сыр",];
+console.log(products)
+
+
+
+// массив книг
+const books = [
+  {
+     name: "100 великих людей ислама",
+     author: "Джихад ат-Турбани",
+     year: "2010",
+     genre: "история",
+ },
+
+  {
+    name: "Гарри Поттер и филосовский камень",
+    author: "Дж. К. Ролинг",
+    year: "1997",
+    genre: "фентази",
+ },
+
+  {
+    name: "Властилин колец",
+    author: "Дж. Р. Р. Толкин",
+    year: "1954",
+    genre: "фентази"
+ }
+];
+
+books.push({
+    name: "Архиологи",
+    author: "Вячеслав Ставецкий",
+    year: "2026",
+    genre: "Архиология",
+});
+
+console.log(books)
+
+
+
+// массив вселеной
+const onePieceBooks = [
+  {
+    name: "One Piece: East Blue",
+     author: "Эйитиро Ода",
+     year: "1997",
+     genre: "Манга",
+ },
+
+  {
+    name: "One Piece: Alabasta",
+     author: "Эйитиро Ода",
+     year: "",
+     genre: "Манга",
+ },
+
+  {
+    name: "One Piece: Wano",
+     author: "Эйитиро Ода",
+     year: "2018",
+     genre: "Манга",
+ }
+];
+
+const allBooks =
+[...books, ...onePieceBooks];
+
+console.log(allBooks);
+
+
+// №10 название книги
+const bookNames = books.map(book =>
+  book.name);
+console.log(bookNames);
--- main.js
+++ main.js
@@ -1,46 +1,48 @@
-// покраска всех карточек
+// покраска  всех карточек
 
 const changeColorCardButton = document.querySelector('#change-Color-all-card');
 const greenColorHash = '#43c160';
 const blueColorHash = '#4A90E2';
 
 changeColorCardButton.addEventListener('click', () => {
-  document.querySelectorAll('.card').forEach((card) => card.style.backgroundColor = greenColorHash)
-})
+    document.querySelectorAll('.card').forEach((card) => {
+    card.style.backgroundColor = greenColorHash;
+    });
+});
 
-// покраска первой карточкиs
+// покраска первой карточки
 
 const firstProductContainers = document.querySelector('.card');
 const changeColorFirstCardButton = document.querySelector('#change-Color-first-card');
 
 changeColorFirstCardButton.addEventListener ('click', () => {
-  firstProductContainers.style.backgroundColor = blueColorHash
+firstProductContainers.style.backgroundColor = blueColorHash
 });
 
 // открытие страницы Google.com
 
-const openGoogleButoon = document.querySelector('#open-google');
+const openGoogleButton = document.querySelector('#open-google');
 const googleURL = 'https://google.com';
-openGoogleButoon.addEventListener('click', openGoogle)
+openGoogleButton.addEventListener('click', openGoogle)
 
-function openGoogle() {
+function openGoogle()  {
   const answer = confirm('вы действительно хотите google?');
 
   if (answer === true) {
     window.open('https://google.com')
-  } else {
+} else {
     return;
-  }
 }
+  }
+
 
 // вывод консоль лог
 const outputlogButton = document.querySelector('#output-console-log');
-  outputlogButton.addEventListener('click', () => outputConsoleLog('дз №6'))
+outputlogButton.addEventListener('click', () => outputConsoleLog('дз №7'))
 
 function outputConsoleLog(message) {
-   console.log(message);
-   alert('10')
-  
+    console.log(message)
+    alert('10')
 }
 
-outputConsoleLog('дз №4')
+outputConsoleLog('дз №7')
